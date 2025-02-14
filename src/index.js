05b781e65b00a75afad09fe5398f99a5b18f3f74
AlmuDiazMadramany <almumadra@hotmail.com>
2025-02-14 11:17:00 +0100

    web

--- src/index.js
+++ src/index.js
@@ -11,3 +11,28 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+
+// endpoint peliculas
+server.get("/movie", (req, res) =>{
+
+  res.json ({
+    success: true,
+        movies: [
+          {
+            id: '1',
+            title: 'Gambita de dama',
+            genre: 'Drama',
+            image:
+              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg'
+          },
+          {
+            id: '2',
+            title: 'Friends',
+            genre: 'Comedia',
+            image:
+              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
+          }
+        ]
+  })
+})
--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -3,29 +3,30 @@
 const getMoviesFromApi = () => {
   console.log('Se están pidiendo las películas de la app');
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json')
+  return fetch('http://localhost:4000/movie')
     .then(response => response.json())
-    .then(() => {
+    .then((data) => {
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
-      return {
-        success: true,
-        movies: [
-          {
-            id: '1',
-            title: 'Gambita de dama',
-            genre: 'Drama',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg'
-          },
-          {
-            id: '2',
-            title: 'Friends',
-            genre: 'Comedia',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
-          }
-        ]
-      };
+      // return {
+      //   success: true,
+      //   movies: [
+      //     {
+      //       id: '1',
+      //       title: 'Gambita de dama',
+      //       genre: 'Drama',
+      //       image:
+      //         '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg'
+      //     },
+      //     {
+      //       id: '2',
+      //       title: 'Friends',
+      //       genre: 'Comedia',
+      //       image:
+      //         '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
+      //     }
+      //   ]
+      // };
+      return data
     });
 };
 
